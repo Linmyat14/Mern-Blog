ba449146b09526c2b99bc4d19f74a6c4087529aa
Linmyat14 <linmyat.n3@gmail.com>
2021-10-17 21:03:04 +0630

    first commit

---
+++ My-Blog/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">Hello, Welcome to my Blog!</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4"> Welcome to my Blog Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae non, tempora minus hic aspernatur inventore dicta itaque. Pariatur, veritatis laborum tenetur ea natus sequi velit enim? Ad aut id unde illum fugiat praesentium, eaque odit nostrum magni mollitia repellat quaerat maiores est expedita odio a laudantium perspiciatis impedit, quae, nemo quasi quisquam. Odio id tempore quae repellendus, accusamus cumque amet sit ipsa minima unde, libero sapiente.</p>
+            <p  className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae non, tempora minus hic aspernatur inventore dicta itaque. Pariatur, veritatis laborum tenetur ea natus sequi velit enim? Ad aut id unde illum fugiat praesentium, eaque odit nostrum magni mollitia repellat quaerat maiores est expedita odio a laudantium perspiciatis impedit, quae, nemo quasi quisquam. Odio id tempore quae repellendus, accusamus cumque amet sit ipsa minima unde, libero sapiente.</p>
+            <p  className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae non, tempora minus hic aspernatur inventore dicta itaque. Pariatur, veritatis laborum tenetur ea natus sequi velit enim? Ad aut id unde illum fugiat praesentium, eaque odit nostrum magni mollitia repellat quaerat maiores est expedita odio a laudantium perspiciatis impedit, quae, nemo quasi quisquam. Odio id tempore quae repellendus, accusamus cumque amet sit ipsa minima unde, libero sapiente.Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae non, tempora minus hic aspernatur inventore dicta itaque. Pariatur, veritatis laborum tenetur ea natus sequi velit enim? Ad aut id unde illum fugiat praesentium, eaque odit nostrum magni mollitia repellat quaerat maiores est expedita odio a laudantium perspiciatis impedit, quae, nemo quasi quisquam. Odio id tempore quae repellendus, accusamus cumque amet sit ipsa minima unde, libero sapiente.</p>
+        </div>
+    )
+}
+
+export default Home
